351efd3488235f5ab8ba2d3730b81aa3e6acdc1b
Elizabeth Vasquez <vasquezelizabeth03@gmail.com>
2019-09-20 13:52:44 -0500

    implemented small calendar

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React,{Component} from 'react';
 import './App.css';
-
-function App() {
-  return (
-    <div></div>
-  );
+import Calendar from 'react-calendar';
+ 
+class App extends Component {
+  state = {
+    date: new Date(),
+  }
+ 
+  onChange = date => this.setState({ date })
+ 
+  render() {
+    return (
+      <div>
+        <Calendar
+          onChange={this.onChange}
+          value={this.state.date}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
