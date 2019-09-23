1a49f2a9995b4861f7d65c2fc94265ea49fa25fd
Elizabeth Vasquez <vasquezelizabeth03@gmail.com>
2019-09-23 11:09:56 -0500

    added clientid as fk to ptsessions table

--- server/models/ptsession.js
+++ server/models/ptsession.js
@@ -7,7 +7,7 @@ module.exports = (sequelize, DataTypes) => {
     allDay: DataTypes.BOOLEAN
   }, {});
   PtSession.associate = function(models) {
-    // associations can be defined here
+
   };
   return PtSession;
 };
