b440c63aeb33c461f16235a05168b18c393f7395
Elizabeth Vasquez <vasquezelizabeth03@gmail.com>
2019-09-23 09:29:43 -0500

    added link to add appt

--- client/src/components/dashboard/Nav.js
+++ client/src/components/dashboard/Nav.js
@@ -3,7 +3,6 @@ import Paper from '@material-ui/core/Paper';
 import MenuItem from '@material-ui/core/MenuItem';
 import MenuList from '@material-ui/core/MenuList';
 import { makeStyles } from '@material-ui/core/styles';
-import Avatar from '@material-ui/core/Avatar';
 import HomeIcon from '@material-ui/icons/Home';
 import DateRangeIcon from '@material-ui/icons/DateRange';
 import PermContactCalendarIcon from '@material-ui/icons/PermContactCalendar';
