0a11ac2ecf9f8618b9e3a4e278a35968bc2a48a3
Elizabeth Vasquez <vasquezelizabeth03@gmail.com>
2019-09-25 23:32:36 -0500

    base layout

--- client/src/App.js
+++ client/src/App.js
@@ -4,13 +4,41 @@ import CancelAppt from './components/pt/CancelAppt'
 
 
 
-class App extends Component {
- 
-  render() {
+const App =()=>{
+  
+  let appts = [{title:'Client name'}, {time: '9:30'}]
+  
+  
     return (
-      <div><CancelAppt/></div>
+      <React.Fragment>
+      <div id="welcome">
+        <h1>Welcome, (name)</h1>
+      </div>
+      <div class="grid-container">
+        <div class="Upcoming-Appts">
+          <h3>Upcoming Appointments</h3>
+          {appts.map(appt=>{
+            return appt.title
+          })}
+        </div>
+        <div class="child">
+          <h3>Tasks</h3>
+        </div>
+        <div class="child">
+          <h3>Messages</h3>
+        </div>
+        <div class="child">
+          <h3>Revenue</h3>
+        </div>
+        <div class="child">
+          <h3>Settings</h3>
+        </div>
+      </div>
+      </React.Fragment>
+
+
     )
-}
+
 }
 
 export default App;
